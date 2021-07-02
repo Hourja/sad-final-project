22e3987713f785783f55bb0c3cc5656b5311796b
Amanda Carneiro <49623453+amandabcarneiro9@users.noreply.github.com>
2021-07-02 18:14:19 +0200

    About (#9)
    * starting about us
    
    * about-us finished

--- resources/js/pages/about/About.jsx
+++ resources/js/pages/about/About.jsx
@@ -1,17 +1,44 @@
 import './About.scss'
+import ContactForm from '../../components/ContactForm';
+
 
 export default function  About(){
+const names =  ['Amanda', 'David', 'Spiros']
 
     return (
         
-        <div className="container">
+    <div className="about-us-page">
+        <div className="__about">
             <h1>About Us </h1>
-            <div className="container_content">
-
+            <div className="__developer-list">
+                { names.map(developer)}
             </div>
-            
+         </div>
+        
+        
+        <div className="__getInTouch">
+            <h2>Get In Touch</h2>
+                <div className="__text">
+                    <p>If you would like to find out more about how we can help you, please contact-us trough the form bellow. We welcome your comments and suggestions about this website and/or any other issues that you wish to raise.</p>
+                </div>
+               <ContactForm h2={''}/> 
         </div>
+    </div>
 
         
+    )
+}
+
+function developer(name) {
+    return (
+        <div className="__developer" >
+            
+                    
+                <img src={require('../../../img/profile.png').default} alt="" /> 
+                    <div className="__info">
+                        <h3>{name}</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident accusantium eveniet corporis dolor velit non saepe quibusdam officiis, enim, ullam itaque eaque beatae repellat cupiditate autem illum alias exercitationem similique.</p>
+                    </div>
+            </div>
     )
 }
