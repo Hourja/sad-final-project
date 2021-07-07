0ca269ecb924a946bf79a3d39c6cd042408d97b8
Spyridon Karousos <tgafalkon@gmail.com>
2021-07-07 17:07:45 +0300

    removed api key

--- resources/js/pages/discover/Map.jsx
+++ resources/js/pages/discover/Map.jsx
@@ -1,20 +1,27 @@
 import { Link, useLocation, useParams } from 'react-router-dom'
 export default function Map({ categories }) {
   const { categoryId, city } = useParams()
-  const apiKey = 'AIzaSyCTPsKlX0_d8bIzcVPnmmPBR8fP38m2-Uo'
+  // const apiKey = ''
 
   const category = categories.find((category) => {
     return category.id === Number(categoryId)
   })
 
   return (
+    // <iframe
+    //   width='600'
+    //   height='450'
+    //   loading='lazy'
+    //   allowFullScreen
+    //   src={`https://www.google.com/maps/embed/v1/search?key=${apiKey}
+    // &q=${category.name}+in+${city}&zoom=12`}
+    // ></iframe>
+
     <iframe
-      width='600'
-      height='450'
-      loading='lazy'
-      allowFullScreen
-      src={`https://www.google.com/maps/embed/v1/search?key=${apiKey}
-    &q=${category.name}+in+${city}&zoom=12`}
+      src='https://www.google.com/maps/d/u/0/embed?mid=1HxlZkQPS7UNKW7fVds2CsBx4HPjYeoCb'
+      width='640'
+      height='480'
     ></iframe>
+    // <iframe src="https://www.google.com/maps/d/embed?mid=1z05obk7Met3hFcnQq4RPLqNYqD6r0ZSh&hl=en_US" width="640" height="480"></iframe>
   )
 }
