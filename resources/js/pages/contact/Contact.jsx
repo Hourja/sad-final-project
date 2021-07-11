4d10376b4bf23a10b04467e61ecc42eca8842c19
David <huerta.beltran.david@gmail.com>
2021-07-11 02:20:39 +0200

    header, mobile menu, dark mode finalized

--- resources/js/pages/contact/Contact.jsx
+++ resources/js/pages/contact/Contact.jsx
@@ -6,8 +6,8 @@ function Contact() {
       h2={'Contact us'}
       children={
         <div className='contact__text'>
-          If you would like to find out more about how we can help you, please contact-us trough the form bellow. We
-          welcome your comments and suggestions about this website and/or any other issues that you wish to raise.
+          <p>If you would like to find out more about how we can help you, please contact-us trough the form bellow.</p> 
+          <p>We welcome your comments and suggestions about this website and/or any other issues that you wish to raise.</p>
         </div>
       }
     />
