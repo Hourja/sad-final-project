03727e322d0e4111e120c786e30d030d423cbff1
David <huerta.beltran.david@gmail.com>
2021-07-12 23:21:59 +0200

    hamburger menu new design added

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -7,24 +7,13 @@ import UserContext from '../UserContext'
 import Container from '../components/container/Container'
 import './header.scss'
 import BackTopButton from './BackTopButton'
+import Hamburger from './Hamburger'
 import Switch from './dark mode/Switch'
 import { faWindowClose as Close } from '@fortawesome/free-solid-svg-icons';
 import { faAlignJustify as Open } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   const { loggedIn } = useContext(UserContext)
-
-  const [hamburgerMenu, showHamburger] = useState(false);
-  const [open, setOpen] = useState(Open);
-
-  const toggleMenu = () => {
-    hamburgerMenu ? showHamburger(false) : showHamburger(true);
-    if(open == Open) {
-			setOpen(Close)
-		} else (
-			setOpen(Open)
-		)
-  }
 
   return (
     <header className='header'>
@@ -73,47 +62,7 @@ function Header() {
             )}
           </ul>
         </nav>
-        <div className='hamburger'><FontAwesomeIcon onClick={toggleMenu} icon={open} size="3x"/></div>
-        <nav className={'mobile-menu' + (hamburgerMenu ? ' visible-hamburger' : '')}>
-        <ul className='nav-bar'>
-            <li className='home-link'>
-              <Link to='/'>
-                <FontAwesomeIcon icon='home' /> Home
-              </Link>
-            </li>
-            <li className='cities-link-mobile'>
-              Cities
-              <ul className='dropdown-mobile'>
-                <li className='city-dropdown-mobile'>
-                  <Link to='/city/santorini' className='city-list-dropdown-mobile'>Santorini</Link>
-                  <Link to='/city/rio-de-janeiro' className='city-list-dropdown-mobile'>Rio de Janeiro</Link>
-                  <Link to='/city/warsaw' className='city-list-dropdown-mobile'>Warsaw</Link>
-                </li>
-              </ul>
-            </li>
-            <li className='about-link'>
-              <Link to='/about-us'>About</Link>
-            </li>
-            <li className='contact-link'>
-              <Link to='/contact'>Contact</Link>
-            </li>
-            {!loggedIn && (
-              <li className='register-link'>
-                <Link to='/sign-up'>Register</Link>
-              </li>
-            )}
-            {!loggedIn && (
-              <li className='login-link'>
-                <Link to='/sign-in'>Login</Link>
-              </li>
-            )}
-            {loggedIn && (
-              <li className='logout-link'>
-                <Link to='/sign-out'>Logout</Link>
-              </li>
-            )}
-          </ul>
-        </nav>
+        <Hamburger />
         <Switch />
       </Container>
       <BackTopButton />
