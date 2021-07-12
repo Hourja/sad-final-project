a751a86577e2fe381ad11aadbc5c683e4c81da6a
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-07-12 15:33:01 +0200

    solving conflicts

--- resources/js/requests/fetchCity.js
+++ resources/js/requests/fetchCity.js
@@ -1,5 +1,5 @@
-export default async function fetchCity(slug) {
-  const url = `/api/cities/${slug}`
+export default async function fetchCity({ slug }) {
+  const url = `api/table/cities`
   const resp = await fetch(url)
   const data = await resp.json()
 
