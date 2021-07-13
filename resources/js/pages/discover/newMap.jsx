80a2d43e8cd95fabdae176a8b4ffc4bd17293a8d
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-07-13 15:07:39 +0200

    remove key from commit

--- resources/js/pages/discover/newMap.jsx
+++ resources/js/pages/discover/newMap.jsx
@@ -3,7 +3,7 @@ export default function Map({ categories }) {
   const { categoryId, city } = useParams()
   const apiKey = ''
 
-  //   url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=formatted_address,geometry,icon,name&locationbias=circle:2000@47.6918452,-122.2226413&key=AIzaSyCTPsKlX0_d8bIzcVPnmmPBR8fP38m2-Uo'
+  //   url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=formatted_address,geometry,icon,name&locationbias=circle:2000@47.6918452,-122.2226413&
 
   const category = categories.find((category) => {
     return category.id === Number(categoryId)
