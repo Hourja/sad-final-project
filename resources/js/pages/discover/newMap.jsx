99b0e0ee72e4647f1c0d8dfa5090ab3f7c3543bc
Amanda Carneiro <amandabcarneiro9@gmail.com>
2021-07-15 10:25:47 +0200

    fixed map

--- resources/js/pages/discover/newMap.jsx
+++ resources/js/pages/discover/newMap.jsx
@@ -1,14 +1,24 @@
 import { Link, useLocation, useParams } from 'react-router-dom'
-
-export default function NewMap({ categories }) {
+export default function Map({ categories }) {
   const { categoryId, city } = useParams()
   const apiKey = ''
-
   const category = categories.find((category) => {
     return category.id === Number(categoryId)
   })
-  const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${category.name}%20in%20${city}&inputtype=textquery&fields=formatted_address,geometry,icon,name&locationbias=circle:2000@47.6918452,-122.2226413&key=${apiKey}`
-
-  console.log(url)
-  return <iframe width='600' height='450' loading='lazy' allowFullScreen src={url}></iframe>
+  return (
+    <iframe
+      width='600'
+      height='450'
+      loading='lazy'
+      allowFullScreen
+      src={`https://www.google.com/maps/embed/v1/search?key=${apiKey}
+    &q=${category.name}+in+${city}&zoom=12`}
+    ></iframe>
+    // <iframe
+    //   src='https://www.google.com/maps/d/u/0/embed?mid=1HxlZkQPS7UNKW7fVds2CsBx4HPjYeoCb'
+    //   width='640'
+    //   height='480'
+    // ></iframe>
+    // <iframe src="https://www.google.com/maps/d/embed?mid=1z05obk7Met3hFcnQq4RPLqNYqD6r0ZSh&hl=en_US" width="640" height="480"></iframe>
+  )
 }
