820d083ccd0ebc8e6335563f07cf85528e792603
David Huerta Beltran <huerta.beltran.david@gmail.com>
2022-01-08 18:26:34 +0100

    latest version of files, and page deployed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      $this->app->bind('path.public', function() {
+        return base_path().'/../public_html';
+      });
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/png" href="/images/favicon.png">
-    <title>welcome traveler</title>
+    <link rel="shortcut icon" sizes="114x114" href="{{ asset('images/favicon.png') }}">
+    <title>DAS Travel App | Discover Local Cities</title>
 
     <link rel="stylesheet" href="{{ mix('js/index.css') }}">
 </head>
